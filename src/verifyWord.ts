7d323a09a1b73d16defdabd80657949153cca859
Leonhard Tissen <contact@warze.org>
2023-12-19 14:40:00 +0100

    First commit

---
+++ src/verifyWord.ts
@@ -0,0 +1,24 @@
+import wordList5 from './wordlists/words_alpha_filtered_5.json';
+import wordList6 from './wordlists/words_alpha_filtered_6.json';
+import wordList7 from './wordlists/words_alpha_filtered_7.json';
+import wordList8 from './wordlists/words_alpha_filtered_8.json';
+import wordList9 from './wordlists/words_alpha_filtered_9.json';
+import wordList10 from './wordlists/words_alpha_filtered_10.json';
+
+const wordListMap: Record<number, Array<string>> = {
+	5: wordList5,
+	6: wordList6,
+	7: wordList7,
+	8: wordList8,
+	9: wordList9,
+	10: wordList10,
+}
+
+function verifyWord(word: string): boolean {
+	const wordList = wordListMap[word.length];
+	return wordList.includes(word.toLowerCase());
+}
+
+console.log(verifyWord('hello'));
+console.log(verifyWord('helloo'));
+console.log(verifyWord('amazing'));
---
+++ words.py
@@ -0,0 +1,27 @@
+# Load src/words_alpha.json
+
+import json
+import os
+
+def load_words():
+	with open(os.path.join(os.path.dirname(__file__), 'src/words_alpha.json')) as word_file:
+		valid_words = json.load(word_file)
+	return valid_words
+
+# Filter out words based on length
+def filter_words(words, charLength):
+	return [word for word in words if len(word) == charLength]
+
+# Save it to a new json at src/words_alpha_filtered.json
+def save_words(words, ending):
+	with open(os.path.join(os.path.dirname(__file__), f'src/words_alpha_filtered_{ending}.json'), 'w') as outfile:
+		json.dump(words, outfile)
+
+if __name__ == '__main__':
+	words = load_words()
+	save_words(filter_words(words, 5), '5')
+	save_words(filter_words(words, 6), '6')
+	save_words(filter_words(words, 7), '7')
+	save_words(filter_words(words, 8), '8')
+	save_words(filter_words(words, 9), '9')
+	save_words(filter_words(words, 10), '10')
