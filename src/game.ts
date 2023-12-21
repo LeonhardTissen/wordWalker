f5048b93cfd12aaf76570a00d95bcfc4b79d8438
Leonhard Tissen <contact@warze.org>
2023-12-21 13:52:10 +0100

    Game prototype

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,45 @@
 import { getHint, verifyWord } from "./verifyWord";
 
-const cvs = document.getElementById("game") as HTMLCanvasElement;
-const ctx = cvs.getContext("2d") as CanvasRenderingContext2D;
+enum Direction {
+	Up,
+	Down,
+	Right,
+	Left,
+}
 
 let currentWord: string = '';
 let targetLength: number = 5;
+let currentDirection: Direction = Direction.Right;
+let knowsAboutGoingLeft: boolean = false;
+
+const nextDirections: Array<Direction> = [
+	Direction.Right,
+	Direction.Down,
+	Direction.Right,
+	Direction.Up,
+	Direction.Right,
+	Direction.Down,
+	Direction.Right,
+	Direction.Up,
+	Direction.Right,
+	Direction.Down,
+	Direction.Right,
+	Direction.Down,
+	Direction.Right,
+	Direction.Down,
+	Direction.Left,
+	Direction.Down,
+	Direction.Left,
+	Direction.Up,
+	Direction.Left,
+	Direction.Down,
+	Direction.Left,
+	Direction.Down,
+	Direction.Right,
+	Direction.Down,
+];
+let currentPosX: number = 0;
+let currentPosY: number = 50;
 
 const wordHistory: Array<string> = [];
 
@@ -13,30 +48,74 @@ const validCharacters = 'abcdefghijklmnopqrstuvwxyz';
 document.body.addEventListener('keydown', (e) => {
 	if (e.key === 'Backspace' && currentWord.length > 1) {
 		currentWord = currentWord.slice(0, -1);
+
+		emptyLetterBox(currentPosX, currentPosY);
+		switch (currentDirection) {
+			case Direction.Up:
+				currentPosY ++;
+				break;
+			case Direction.Down:
+				currentPosY --;
+				break;
+			case Direction.Right:
+				currentPosX --;
+				break;
+			case Direction.Left:
+				currentPosX ++;
+				break;
+		}
+		updateCameraPos();
 	}
 	else if (validCharacters.includes(e.key) && currentWord.length < targetLength) {
 		currentWord += e.key;
+		switch (currentDirection) {
+			case Direction.Up:
+				currentPosY --;
+				break;
+			case Direction.Down:
+				currentPosY ++;
+				break;
+			case Direction.Right:
+				currentPosX ++;
+				break;
+			case Direction.Left:
+				currentPosX --;
+				break;
+		}
+		updateCameraPos();
+		fillLetterBox(currentPosX, currentPosY, e.key);
 	}
 
-	console.log(currentWord.padEnd(targetLength, '_'));
-
 	if (currentWord.length === targetLength) {
-		const isValidWord = verifyWord(currentWord);
+		const wordToCheck = currentDirection === Direction.Left ? currentWord.split('').reverse().join('') : currentWord;
 
-		if (isValidWord && !wordHistory.includes(currentWord)) {
-			// Green text console log
-			console.log(`%cCorrect`, 'color: green');
-			wordHistory.push(currentWord);
+		const isValidWord = verifyWord(wordToCheck);
 
-			// Keep last letter
-			currentWord = currentWord.slice(-1);
-			console.log(currentWord.padEnd(targetLength, '_'));
+		if (!isValidWord) {
 
-			// Change target length to 5 - 10
-			targetLength = Math.floor(Math.random() * 6) + 5;
-			console.log('%cNew target length: ' + targetLength, 'color: yellow')
+			alert(`${wordToCheck} is not a valid word!`);
+
 		} else {
-			console.log(`%cFalse`, 'color: red');
+
+			if (wordHistory.includes(wordToCheck)) {
+
+				alert(`${wordToCheck} has already been used!`);
+
+			} else {
+				
+				wordHistory.push(wordToCheck);
+				currentDirection = nextDirections[wordHistory.length % nextDirections.length];
+				
+				// Keep last letter
+				currentWord = currentWord.slice(-1);
+				
+				// Change target length to 5 - 10
+				targetLength = Math.floor(Math.random() * 6) + 5;
+	
+				spawnLetterBoxSnake(currentPosX, currentPosY, targetLength, currentDirection);
+
+			}
+
 		}
 	}
 
@@ -44,20 +123,78 @@ document.body.addEventListener('keydown', (e) => {
 	if (hint !== undefined) {
 		console.log(`Hint: ${hint}`);
 	}
+
+	if (currentDirection === Direction.Left && !knowsAboutGoingLeft) {
+		alert('When going left, you need to type the word backwards and it needs to end with the last letter of the previous word');
+		knowsAboutGoingLeft = true;
+	}
 });
 
-function updateScreen() {
-	cvs.width = window.innerWidth;
-	cvs.height = window.innerHeight;
+const game = document.getElementById('game') as HTMLDivElement;
+
+const letterBoxSize = 50;
+
+function getBoxName(x: number, y: number): string {
+	return `letterBox-${x}-${y}`;
 }
 
-updateScreen();
-window.addEventListener("resize", updateScreen);
-
-function draw() {
-	ctx.clearRect(0, 0, cvs.width, cvs.height);
-
-	requestAnimationFrame(draw);
+function spawnEmptyLetterBox(x: number, y: number): void {
+	const existingLetterBox = document.getElementById(getBoxName(x, y));
+	if (existingLetterBox) {
+		return;
+	}
+	const letterBox = document.createElement('div');
+	letterBox.classList.add('letterBox');
+	letterBox.id = getBoxName(x, y);
+	letterBox.style.left = `${x * letterBoxSize}px`;
+	letterBox.style.top = `${y * letterBoxSize}px`;
+	game.appendChild(letterBox);
 }
 
-draw();
+function fillLetterBox(x: number, y: number, letter: string): void {
+	const letterBox = document.getElementById(getBoxName(x, y));
+	if (!letterBox) {
+		return;
+	}
+	letterBox.innerText = letter;
+}
+
+function emptyLetterBox(x: number, y: number): void {
+	const letterBox = document.getElementById(getBoxName(x, y));
+	if (!letterBox) {
+		return;
+	}
+	letterBox.innerText = '';
+}
+
+function spawnLetterBoxSnake(x: number, y: number, length: number, direction: Direction): void {
+	for (let i = 0; i < length; i++) {
+		switch (direction) {
+			case Direction.Up:
+				spawnEmptyLetterBox(x, y - i);
+				break;
+			case Direction.Down:
+				spawnEmptyLetterBox(x, y + i);
+				break;
+			case Direction.Right:
+				spawnEmptyLetterBox(x + i, y);
+				break;
+			case Direction.Left:
+				spawnEmptyLetterBox(x - i, y);
+				break;
+		}
+	}
+}
+
+function setCurrentPos(x: number, y: number): void {
+	currentPosX = x;
+	currentPosY = y;
+	updateCameraPos();
+}
+
+function updateCameraPos(): void {
+	game.style.transform = `translate(-${currentPosX * letterBoxSize}px, -${currentPosY * letterBoxSize}px)`;
+}
+
+spawnLetterBoxSnake(currentPosX + 1, currentPosY, targetLength, nextDirections[0]);
+updateCameraPos();
