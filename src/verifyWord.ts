a4ba9ef5898cd77d9505ccbc0f9668d1fee8f51e
Leonhard Tissen <contact@warze.org>
2024-01-02 11:23:47 +0100

    Refactor API URLs in verifyWord.ts

--- src/verifyWord.ts
+++ src/verifyWord.ts
@@ -1,11 +1,13 @@
+const api = 'https://wild-erin-harp-seal-vest.cyclic.app/ww';
+
 export async function verifyWord(word: string): Promise<boolean> {
-	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/check?word=${word}`);
+	const response = await fetch(`${api}/check?word=${word}`);
 	const responseObject = await response.json();
 	return responseObject.result;
 }
 
 export async function getHint(wordStart: string, totalLength: number, isReverse: boolean = false): Promise<string | undefined> {
-	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/match?word=${wordStart}&length=${totalLength}&reverse=${isReverse}`);
+	const response = await fetch(`${api}/match?word=${wordStart}&length=${totalLength}&reverse=${isReverse}`);
 	const responseObject = await response.json();
 	return responseObject.matching_word || responseObject.message;
 }
