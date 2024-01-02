0da1ac6c41b091cc0ff7c07eee98c8f4f67556b0
Leonhard Tissen <contact@warze.org>
2024-01-02 11:22:35 +0100

    Refactor verifyWord and getHint functions to use JSON response

--- src/verifyWord.ts
+++ src/verifyWord.ts
@@ -1,11 +1,11 @@
 export async function verifyWord(word: string): Promise<boolean> {
 	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/check?word=${word}`);
-	const text = await response.text();
-	return text === 'true';
+	const responseObject = await response.json();
+	return responseObject.result;
 }
 
 export async function getHint(wordStart: string, totalLength: number, isReverse: boolean = false): Promise<string | undefined> {
 	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/match?word=${wordStart}&length=${totalLength}&reverse=${isReverse}`);
-	const text = await response.text();
-	return text === 'No matching word found' ? undefined : text;
+	const responseObject = await response.json();
+	return responseObject.matching_word || responseObject.message;
 }
