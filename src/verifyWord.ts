866ff58e79e00e28a77bc3c2b24ddd5333cb66de
Leonhard Tissen <contact@warze.org>
2024-01-02 11:14:25 +0100

    Update API endpoints

--- src/verifyWord.ts
+++ src/verifyWord.ts
@@ -1,11 +1,11 @@
 export async function verifyWord(word: string): Promise<boolean> {
-	const response = await fetch(`https://warze.org/wordwalker/checkword?word=${word}`);
+	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/check?word=${word}`);
 	const text = await response.text();
 	return text === 'true';
 }
 
 export async function getHint(wordStart: string, totalLength: number, isReverse: boolean = false): Promise<string | undefined> {
-	const response = await fetch(`https://warze.org/wordwalker/getmatchingword?word=${wordStart}&length=${totalLength}&reverse=${isReverse}`);
+	const response = await fetch(`https://wild-erin-harp-seal-vest.cyclic.app/ww/match?word=${wordStart}&length=${totalLength}&reverse=${isReverse}`);
 	const text = await response.text();
 	return text === 'No matching word found' ? undefined : text;
 }
